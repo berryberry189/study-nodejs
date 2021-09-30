102bba9865d421757f15c510c893df44ef93f5ad
berryberry189 <berryberry189@naver.com>
2021-09-30 23:18:02 +0900

    코드 리펙토링 - 테스트 환경 개선

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ const port = 3000;
 const bodyParser = require('body-parser');
 const user = require('./api/user');
 
-app.use(morgan('dev'));
+if(process.env.NODE_ENV !== 'test'){
+    // 테스트가 아닐 경우에만 서버 로그를 찍음
+    app.use(morgan('dev'));
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/users', user);
-
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-});
 
 module.exports = app;
